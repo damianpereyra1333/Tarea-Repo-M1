32c87f1ec4fa89d89b9001143cd365037d672ca6
damianpereyra1333 <damiangoyano@gmail.com>
2025-10-18 18:48:46 -0300

    actulizado

--- repo M1/Proyecto1/scripts/index.js
+++ repo M1/Proyecto1/scripts/index.js
@@ -16,7 +16,7 @@ class repositorio{
         return this.pedidos;
     }
     crearPedido(nombre, detalles, urlimagen){
-        const pedido=new Pedidos(this.contador, nombre, detalles, urlimagen);
+        const pedido=new pedido (this.contador, nombre, detalles, urlimagen);
         this.pedidos.push(pedido);
         this.contador++;
     } 
@@ -31,10 +31,36 @@ class repositorio{
         const descripcion=document.getElementById("descripcion").value;
         const url=document.getElementById("url").value;
 
-        if(titulos==="" || descripcion==="" || url===""){
+        if(titulo==="" || descripcion==="" || url===""){
             alert("Por favor complete todos los campos");
             return;
         }
         repositorio.crearPedido(titulo, descripcion, url);
-        console.log(repositorio.mostrarPedidos());
+        
+        const almacen=document.getElementById("almacen");
+        almacen.innerHTML="";
+
+        const tarjeta=repositorio.mostrarPedidos().map(({id, nombre, detalles, urlimagen})=>{
+            const div=document.createElement("div");
+            const tit=document.createElement("h2");
+            const img=document.createElement("img");
+            const descr=document.createElement("p");
+
+            tit.innerHTML=nombre;
+            img.src=urlimagen;
+            descr.innerHTML=detalles;
+
+            div.appendChild(tit);
+            div.appendChild(img);
+            div.appendChild(descr);
+
+            return div;
+
+
+        })
+         
+        tarjeta.forEach (div=> almacen.appendChild(div));
+            
+        
+
         });
